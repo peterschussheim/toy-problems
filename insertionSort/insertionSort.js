b50a426dd9ffad67df4d1cfefee86660aaf63d5c
Peter Schussheim <peter@schussheim.com>
2017-03-31 16:06:59 -0400

    commit before start new problem

--- insertionSort/insertionSort.js
+++ insertionSort/insertionSort.js
@@ -1,5 +1,10 @@
 const insertionSort = array => {
-  return array
+  // mark first element as sorted
+  // for each unsorted element
+    // extract element[i]
+    // iterate until end
+      // if currentsorted ? extracted
+        // move start to right by 
 }
 
 export default insertionSort
